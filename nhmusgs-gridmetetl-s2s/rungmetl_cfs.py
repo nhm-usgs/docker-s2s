e57b60bbbf5fb18709264f56c4c72ebbd1229ce8
Richard McDonald <rmcd@usgs.gov>
2021-04-08 18:25:03 -0600

    Got the environment set up, and made some mods to full full 48 ensemble and calc median and mean, because provided median only start on tomorrow will not provide continuous record with regular gridmet, but 48 enembles will.  Starting with median for ease in testing before moving on to running full ensembles

---
+++ nhmusgs-gridmetetl-s2s/rungmetl_cfs.py
@@ -0,0 +1,114 @@
+import gridmet_cfsv2 as gm
+import grd2shp
+import geopandas as gpd
+import argparse
+from pathlib import Path
+import sys
+
+
+def valid_path(s):
+    if Path(s).exists():
+        return s
+    else:
+        raise argparse.ArgumentError(f'Path does not exist: {s}')
+
+
+def valid_file(s):
+    if Path(s).exists():
+        return s
+    else:
+        raise argparse.ArgumentError(f'File does not exist: {s}')
+
+
+def parser():
+    my_parser = argparse.ArgumentParser(prog='runGMETL_CFSv2',
+                                        description='ETL for gridMET CFSv2 (NOAA)')
+
+    my_parser.add_argument('-t', '--type_extract', type=str,
+                           help='extraction method: (median) or (enseble)', metavar='extraction type',
+                           default='median', required=False, choices=['median', 'ensemble'])
+
+    my_parser.add_argument('-s', '--shape_file', type=valid_file,
+                           help='path/file.shp - path to shapefile', metavar='path/shapefile.shp',
+                           default=None, required=True)
+
+    my_parser.add_argument('-w', '--weights_file', type=valid_file,
+                           help='path/weights.csv - path to weights file', metavar='path/weights.csv',
+                           default=None, required=True)
+
+    my_parser.add_argument('-o', '--outpath', type=valid_path,
+                           help='Output path (location of netcdf output files)', metavar='output_path',
+                           default=None, required=True)
+
+    my_parser.add_argument('-e', '--elev_file', type=valid_file,
+                           help='path/elev.gpkg - path to elevation file, used to convert specific humidity ' +
+                                'to relative humidity',
+                           metavar='path/elev.gpkg',
+                           default=None, required=True)
+    return my_parser
+
+
+def args(parser):
+    return parser.parse_args()
+
+
+def main():
+    my_parser = parser()
+    my_args = args(my_parser)
+
+    if my_args.type_extract == 'median':
+        etype = 0
+    else:
+        etype = 1
+
+    shpf = my_args.shape_file
+    wghtf = my_args.weights_file
+    elevf = my_args.elev_file
+    opth = my_args.outpath
+
+    gm_vars = ['air_temperature',
+               'air_temperature',
+               'precipitation_amount',
+               'wind_speed',
+               'surface_downwelling_shortwave_flux_in_air',
+               'specific_humidity']
+
+    m = gm.Gridmet(type=etype)
+    if etype == 0:
+        ds1 = m.tmax
+        ds2 = m.tmin
+        ds3 = m.prcp
+        ds4 = m.wind_speed
+        ds5 = m.srad
+        ds6 = m.specific_humidity
+    elif etype == 1:
+        ds1 = m.tmax.median(dim='time')
+        ds2 = m.tmin.median(dim='time')
+        ds3 = m.prcp.mean(dim='time')
+        ds4 = m.wind_speed.median(dim='time')
+        ds5 = m.srad.median(dim='time')
+        ds6 = m.specific_humidity.median(dim='time')
+
+    dat = [ds1, ds2, ds3, ds4, ds5, ds6]
+    vout = ['tmax', 'tmin', 'prcp', 'ws', 'srad', 'shum']
+    gdf = gpd.read_file(shpf)
+    g2s = grd2shp.Grd2Shp()
+    g2s.initialize(grd=dat,
+                   calctype=etype,
+                   shp=gdf,
+                   wght_file=wghtf,
+                   time_var='day',
+                   lat_var='lat',
+                   lon_var='lon',
+                   var=gm_vars,
+                   var_output=vout,
+                   opath=opth,
+                   fileprefix='f_')
+    for i in range(1):
+        g2s.run_weights()
+
+    g2s.write_file(elev_file=elevf, punits=1)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
